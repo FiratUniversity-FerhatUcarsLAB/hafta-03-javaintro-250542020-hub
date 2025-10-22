af7fc233cb2e7ee33abcec96659edce555920ec3
250542020-hub <250542020@firat.edu.tr>
2025-10-22 15:20:29 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+
+             String ad = " Ahmet Enes ";
+             String soyad = " Altun ";
+             String ogrNo = " 250542020 ";
+             String bolum = " Yazilim Muh .";
+
+             System.out.println(" =================== ");
+             System.out.println(" OGRENCI BILGILERI ");
+             System.out.println(" =================== ");
+             System.out.println(" Ad : " + ad );
+             System.out.println(" Soyad : " + soyad );
+             System.out.println(" No : " + ogrNo );
+             System.out.println(" Bolum : " + bolum );
+             System.out.println(" =================== ");
+    }
+}
