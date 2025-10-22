605ce4efb9c575ae4172094e234a72b5a98038ed
250542020-hub <250542020@firat.edu.tr>
2025-10-22 15:13:12 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        double a = 3, b = 4, c = 5;
+
+        double s = (a + b + c) / 2;
+        double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c)); 
+
+        System.out.println("Üçgenin Alanı: " + alan);
+    }
+}
