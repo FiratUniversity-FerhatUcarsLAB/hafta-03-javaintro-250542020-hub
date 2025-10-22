63a1ccbfe18f87796859e25177c6ba6cf23a161e
250542020-hub <250542020@firat.edu.tr>
2025-10-22 14:13:42 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Java'ya hoşgeldin");
+        System.out.println("Java'ya hoşgeldin");
+        System.out.println("Java'ya hoşgeldin");
+        System.out.println("Java'ya hoşgeldin");
+        System.out.println("Java'ya hoşgeldin");
+    }
+}
