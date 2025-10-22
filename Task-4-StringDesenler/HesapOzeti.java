55ccbced5bf671b3d767cb0e9acc16d040c378ca
250542020-hub <250542020@firat.edu.tr>
2025-10-22 16:31:39 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HESAP ÖZETİ");
+        System.out.println("--------------------------");
+        System.out.println("Ürün\t\tMiktar\tFiyat");
+        System.out.println("--------------------------");
+        System.out.println("Ekmek\t\t2\t12.00");
+        System.out.println("Süt\t\t\t1\t25.50");
+        System.out.println("Yumurta\t\t1\t42.00");
+        System.out.println("Elma\t\t3\t36.75");
+        System.out.println("--------------------------");
+        System.out.println("Toplam\t\t\t116.25");
+    }
+}
