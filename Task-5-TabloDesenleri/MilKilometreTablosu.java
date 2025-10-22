73052ca960022a44845756c6079d266bc48ef657
250542020-hub <250542020@firat.edu.tr>
2025-10-22 17:35:25 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double mil;
+        mil = 1.609;
+        System.out.println("bir mil = "+ mil*1 + " km");
+        System.out.println("bir 5 mil = "+ mil*5 + " km");
+        System.out.println("bir 10 mil = "+ mil*10 + " km");
+        System.out.println("bir 20 mil = "+ mil*20 + " km");
+        System.out.println("bir 50 mil = "+ mil*50 + " km");
+
+
+    }
+}
