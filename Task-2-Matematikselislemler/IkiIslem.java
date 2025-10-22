ff3b58a8e04d4acd85a8257a320ca4d1c985d70e
250542020-hub <250542020@firat.edu.tr>
2025-10-22 14:26:34 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+      double pay = (9.5*4.5) - (2.5*3);
+      double payda = 45.5 - 3.5;
+      double bölme = pay / payda;
+      System.out.printf(String.valueOf(bölme));
+    }
+}
