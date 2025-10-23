7d3970c927559b4703be2c3db14fce1a47f131e2
250542020-hub <250542020@firat.edu.tr>
2025-10-23 19:19:46 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  A   H   H  M   M  EEEEE  TTTTT");
+        System.out.println(" A A  H   H  MM MM  E        T  ");
+        System.out.println("AAAA  HHHHH  M M M  EEEE     T  ");
+        System.out.println("A  A  H   H  M   M  E        T  ");
+        System.out.println("A  A  H   H  M   M  EEEEE    T  ");
+    }
+}
