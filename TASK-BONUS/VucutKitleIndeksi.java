746d6f2fcdc7939dce5ab883302d5e6f1a221f0e
250542020-hub <250542020@firat.edu.tr>
2025-10-23 19:26:10 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilo = 62;
+        double boy = 1.65;  
+
+        double bmi = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksi (BMI): " + bmi);
+    }
+}
